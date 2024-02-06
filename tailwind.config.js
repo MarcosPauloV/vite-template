076c8d8590ab7cde40495b1ae5db792d95824ced
MarcosPauloV <marcospvs96@gmail.com>
2024-02-06 10:53:38 -0300

    fix: update tailwidn config for set corect content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js, ts, jsx, tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      
+
     },
   },
   plugins: [],
