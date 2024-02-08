a0b7713c435e3d812fa6ddbed83a2579a317d617
MarcosPauloV <marcospvs96@gmail.com>
2024-02-08 16:23:55 -0300

    fix: fixed a label return in default position after inside content in the input

--- src/presentation/shared/text-field/text-field.tsx
+++ src/presentation/shared/text-field/text-field.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, forwardRef, useCallback } from "react";
+import React, { ChangeEvent, forwardRef, useCallback, useState } from "react";
 import type TextField from "./text-field-types";
 import cltw from "../../utils/cltw";
 import TextFieldMask from "../../utils/text-field-mask";
@@ -27,6 +27,8 @@ Root.displayName = "text-field-root";
 const Input = forwardRef<TextField.Input.IElement, TextField.Input.IProps>(
   ({ onChange, mask, ...props }, ref) => {
 
+    const [inputContent, setInputContent] = useState<boolean>(false);
+
     const onChangeCallback = useCallback((event: ChangeEvent<TextField.Input.IElement>) => {
       if (mask === "cpf") {
         const value = TextFieldMask.cpf(event.target.value);
@@ -46,15 +48,23 @@ const Input = forwardRef<TextField.Input.IElement, TextField.Input.IProps>(
       if (onChange) {
         onChange;
       }
+
+      if (event.target.value === "") {
+        setInputContent(false);
+      } else {
+        setInputContent(true);
+      }
+
     }, [onChange, mask]);
 
     return (
       <input
         data-testid="text-field-input"
+        data-change={inputContent}
         ref={ref}
         className={
           cltw(
-            "peer rounded-md w-full px-4 py-3 text-sm text-gray-600 border-2 focus:bg-white bg-white placeholder:bg-gray-200 placeholder-shown:border-none"
+            "peer rounded-md w-full px-4 py-3 text-sm text-gray-600 border-2 = focus:bg-white bg-white placeholder:bg-gray-200 placeholder-shown:border-none"
           )
         }
         {...props}
@@ -73,7 +83,7 @@ const Label: React.FC<TextField.ILabelProps> = ({ children, htmlFor, ...props })
       {...props}
       htmlFor={htmlFor}
       className={
-        "absolute left-5 top-3.5 px-1 text-sm transition-all bg-white rounded text-gray-500 peer-placeholder-shown:text-sm peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-5 peer-placeholder-shown:left-5 peer-placeholder-shown:bg-gray-200 peer-focus:-top-2.5 peer-focus:left-2 peer-focus:text-blue-500 peer-focus:bg-white peer-focus:font-semibold"
+        "absolute left-5 top-3.5 px-1 text-sm transition-all bg-white rounded text-gray-500 peer-placeholder-shown:text-sm peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-5 peer-placeholder-shown:left-5 peer-placeholder-shown:bg-gray-200 peer-focus:-top-2.5 peer-focus:left-2 peer-focus:text-blue-500 peer-focus:bg-white peer-focus:font-semibold peer-data-[change=true]:-top-2.5 peer-data-[change=true]:left-2"
       }
     >
       {children}
